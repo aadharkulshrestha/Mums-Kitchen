b4a806b6caee18c61034a7e8109b56f08c53fdea
aadharkulshrestha <deepakulshrestha13@gmail.com>
2023-03-14 17:00:09 +0530

    cloud kitchen
    	new file:   README.md
    	new file:   contactus.html
    	new file:   css/style.css
    	new file:   imgs/Group 1.svg
    	new file:   imgs/Screenshot (44).png
    	new file:   imgs/Screenshot (45).png
    	new file:   imgs/Screenshot 2023-03-14 115705.png
    	new file:   imgs/Screenshot 2023-03-14 115741.png
    	new file:   imgs/Screenshot 2023-03-14 115911.png
    	new file:   imgs/Screenshot 2023-03-14 115941.png
    	new file:   imgs/background.jpg
    	new file:   imgs/background1.jpg
    	new file:   imgs/card1.jpg
    	new file:   imgs/card2.png
    	new file:   imgs/card3.png
    	new file:   imgs/cook.png
    	new file:   imgs/delivery boy.png
    	new file:   imgs/delivery man fly.png
    	new file:   imgs/h1.png
    	new file:   imgs/image.png
    	new file:   imgs/location.png
    	new file:   imgs/logo.png
    	new file:   imgs/low-cost.png
    	new file:   imgs/thumb up.png
    	new file:   imgs/tracking.png
    	new file:   index.html
    	new file:   js/index.js
    	new file:   pakage.json

---
+++ js/index.js
@@ -0,0 +1,73 @@
+// function myFunction() {
+//     var ll = document.getElementsByClassName('ll');
+//     var btn = document.getElementById('#myBtn');
+
+//     if (ll.style.display === "none") {
+//         btnText.innerHTML = "Read more";
+//     } else {
+//         ll.style.display = "none";
+//         btnText.innerHTML = "Read less";
+//         moreText.style.display = "inline";
+//     }
+// }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+  
+  function showhideCities() {
+    var moreCities = document.getElementsByClassName('more-cities');
+    var btn = document.getElementById('myBtn');
+  
+    if (btn.innerHTML === 'Read more') {
+      for (let i = 0; i < moreCities.length; i++) {
+        moreCities[i].style.display = 'flex';
+      }
+      btn.innerHTML = 'Read less';
+    } else {
+      for (let i = 0; i < moreCities.length; i++) {
+        moreCities[i].style.display = 'none';
+      }
+      btn.innerHTML = 'Read more';
+    }
+  }
+  
+  function onPageLoad() {
+    var signUpModal = document.getElementById("signUpModal");
+    var signUpBtn = document.getElementById("sign-up");
+    var closeSignUpModal = document.getElementsByClassName("close")[0];
+  
+    var signInModal = document.getElementById("signInModal");
+    var signInBtn = document.getElementById("sign-in");
+    var closeSignInModal = document.getElementsByClassName("close")[1];
+    
+    signUpBtn.onclick = function() {
+      signUpModal.style.display = "block";
+    }
+  
+    signInBtn.onclick = function() {
+      signInModal.style.display = "block";
+    }
+    
+    closeSignUpModal.onclick = function() {
+      signUpModal.style.display = "none";
+    }
+  
+    closeSignInModal.onclick = function() {
+      signInModal.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+      if (event.target == signUpModal) {
+        signUpModal.style.display = "none";
+      }
+      if (event.target == signInModal) {
+        signInModal.style.display = "none";
+      }
+    }
+  }
